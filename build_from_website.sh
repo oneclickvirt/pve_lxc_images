039e5044038ef590ae23d16dda42586458689fa7
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-05 16:45:41 +0800

    Update build_from_website.sh

--- build_from_website.sh
+++ build_from_website.sh
@@ -129,7 +129,7 @@ for ((i=0; i<${#images[@]}; i++)); do
             pct exec $CTID -- yum install -y dos2unix curl
             pct exec $CTID -- yum install -y curl
             pct exec $CTID -- yum clean packages
-            pct exec $CTID -- curl -lk https://gitee.com/SuperManito/LinuxMirrors/raw/main/ChangeMirrors.sh -o ChangeMirrors.sh
+            pct exec $CTID -- curl -lk https://raw.githubusercontent.com/SuperManito/LinuxMirrors/main/ChangeMirrors.sh -o ChangeMirrors.sh
             pct exec $CTID -- chmod 777 ChangeMirrors.sh
             pct exec $CTID -- ./ChangeMirrors.sh --use-official-source --web-protocol http --intranet false --close-firewall true --backup true --updata-software false --clean-cache false --install-epel true --ignore-backup-tips
         
