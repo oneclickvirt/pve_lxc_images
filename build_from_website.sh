c47715e9c3950b458cbad69f052c44daef4e058d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-05 20:22:34 +0800

    Update build_from_website.sh

--- build_from_website.sh
+++ build_from_website.sh
@@ -68,6 +68,7 @@ failed_images=(
     "debian-10-standard_10.5-1_amd64"
     "debian-10-standard_10.7-1_amd64"
     "debian-10.0-standard_10.0-1_amd64"
+    "debian-6.0-standard_6.0-7_amd64"
 )
 while IFS= read -r line; do
   failed_image=false
