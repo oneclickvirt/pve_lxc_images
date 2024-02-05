7f24a82e5a03ab5f16bcfe26dd9222d5a1daf69a
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-05 16:14:54 +0800

    Update build_from_pveam.sh

--- build_from_pveam.sh
+++ build_from_pveam.sh
@@ -179,7 +179,7 @@ for ((i=0; i<${#images[@]}; i++)); do
     vzdump $CTID --dumpdir /root/temp --compress zstd
     rm -rf /root/temp/*.log
     backup_file_name=$(ls /root/temp | grep "vzdump")
-    real_name=$(basename "${image}")
+    real_name=$(echo "${image}" | cut -d'.' -f1)
     mv /root/temp/${backup_file_name} /root/fixed/${real_name}.tar.zst
     rm -rf /root/temp/vzdump*
     pct destroy $CTID
