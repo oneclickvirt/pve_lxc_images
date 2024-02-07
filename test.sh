9f3baa867fbbaa70db7c2c88032144f8bf746236
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-07 22:20:28 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,12 +11,13 @@ done
 images=($(echo "$response" | jq -r '.assets[].name'))
 for image in "${images[@]}"; do
   echo "$image"
+  echo "$image" >> log
   pct create 102 "$image" -cores 6 -cpuunits 1024 -memory 26480 -swap 0 -rootfs local:10 -onboot 1 -features nesting=1
   pct start 102
   pct set 102 --hostname 101
   res0=$(pct set 102 --net0 name=eth0,ip=172.16.1.3/24,bridge=vmbr1,gw=172.16.1.1)
   if [[ $res0 == *"error"* ]]; then
-      echo "$image set eth0 failed" >> log
+      echo "set eth0 failed" >> log
   fi
   pct set 102 --nameserver 1.1.1.1
   pct set 102 --searchdomain local
@@ -25,32 +26,33 @@ for image in "${images[@]}"; do
   if [[ $res1 == *"command not found"* ]]; then
       res1=$(pct exec 102 -- ${manager} install -y lsof)
   else
-      echo "$image no lsof" >> log
+      echo "no lsof" >> log
   fi
   sleep 1
   res1=$(pct exec 102 -- lsof -i:22)
   if [[ $res1 == *"ssh"* ]]; then
       echo "ssh config correct"
   else
-      echo "$image no ssh" >> log
+      echo "no ssh" >> log
   fi
   res2=$(pct exec 102 -- curl --version)
   if [[ $res2 == *"command not found"* ]]; then
-      echo "$image no curl" >> log
+      echo "no curl" >> log
   fi
   res3=$(pct exec 102 -- wget --version)
   if [[ $res3 == *"command not found"* ]]; then
-      echo "$image no wget" >> log
+      echo "no wget" >> log
   fi
   echo "nameserver 8.8.8.8" | pct exec 102 -- tee -a /etc/resolv.conf
   res4=$(pct exec 102 -- curl -lk https://raw.githubusercontent.com/spiritLHLS/ecs/main/back/test)
   if [[ $res4 == *"success"* ]]; then
       echo "network is public"
   else
-      echo "$image no network" >> log
+      echo "no public network" >> log
   fi
   pct stop 102
   pct destroy 102
+  echo "------------------------------------------" >> log
 done
 
 
