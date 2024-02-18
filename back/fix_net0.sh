e9d7552714341355934a928013bfbd2e716460b0
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-02-18 11:01:09 +0800

    Update fix_net0.sh

--- back/fix_net0.sh
+++ back/fix_net0.sh
@@ -6,10 +6,11 @@ for release_name in *; do
       echo "$release_name"
       pct create 101 "$release_name" -cores 6 -cpuunits 1014 -memory 26480 -swap 0 -rootfs local:10 -onboot 1 -features nesting=1
       pct start 101
-      pct set 101 --net0 name=spiritlhl,ip=172.16.1.2/24,bridge=vmbr1,gw=172.16.1.1
+      pct set 101 --net0 name=spiritlhl,ip=172.16.1.111/24,bridge=vmbr1,gw=172.16.1.1
       pct set 101 --delete net0
       pct exec 101 -- ip addr flush dev eth0
       pct exec 101 -- ip addr flush dev spiritlhl
+      pct exec 101 -- rm -rf /etc/network/.pve-ignore.interfaces
       pct stop 101
       vzdump 101 --dumpdir /root/tp --compress zstd
       rm -rf /root/tp/*.log
